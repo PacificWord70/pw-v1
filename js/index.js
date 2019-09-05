5e5700117cf7fb589e54585cdf1723fa69c901a6
PacificWord70 <heidianderson@sfhs.com>
2019-09-05 17:38:47 -0400

    updated work experience and about me

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ document.querySelector('#cs251').addEventListener('click', cs251)
 document.querySelector('#cs252').addEventListener('click', cs252)
 document.querySelector('#cs373').addEventListener('click', cs373)
 document.querySelector('#cs348').addEventListener('click', cs348)
+document.querySelector('#cs381').addEventListener('click', cs381)
+document.querySelector('#cs448').addEventListener('click', cs448)
+//document.querySelector('#cs471').addEventListener('click', cs471)
+document.querySelector('#stat350').addEventListener('click', stat350)
+//document.querySelector('#stat416').addEventListener('click', stat416)
 
 function cs180() {
     const item = this.parentElement.querySelector('#cs180')
@@ -161,5 +166,110 @@ function cs348() {
         `
     } else {
         item.innerHTML='▾ CS348 : Information Systems'
+    }  
+}
+
+function cs381() {
+    const item = this.parentElement.querySelector('#cs381')
+    console.log(item.innerHTML)
+    if(item.innerHTML=='▾ CS381 : The Analysis of Algorithms')
+    {
+        item.innerHTML = 
+        `
+            <v>▸ CS381 : The Analysis of Algorithms
+            <h6 style="border-left: 1px solid lightgrey; padding-left: 5pt;"> Introduction to the design 
+            and analysis of algorithms. A tentative list of topics includes: sorting and order 
+            statistics; common algorithm design techniques, including divide-and-conquer, 
+            dynamic programming, and greedy; design and use of data structures; flows and cuts; 
+            lower bound techniques; graph algorithms; NP-completeness; randomized algorithms; 
+            approximation algorithms. </h6>
+        `
+    } else {
+        item.innerHTML='▾ CS381 : The Analysis of Algorithms'
+    }  
+}
+
+function cs448() {
+    const item = this.parentElement.querySelector('#cs448')
+    console.log(item.innerHTML)
+    if(item.innerHTML=='▾ CS448 : Relational Database Systems')
+    {
+        item.innerHTML = 
+        `
+            <v>▸ CS448 : Relational Database Systems
+            <h6 style="border-left: 1px solid lightgrey; padding-left: 5pt;"> An in-depth 
+            examination of relational database systems including theory and concepts as 
+            well as practical issues in relational databases. Modern database technologies 
+            such as object-relational and Web-based access to relational databases. 
+            Conceptual design and entity relationship modeling, relational algebra and 
+            calculus, data definition and manipulation languages using SQL, schema and 
+            view management, query processing and optimization, transaction management, 
+            security, privacy, integrity management. </h6>
+        `
+    } else {
+        item.innerHTML='▾ CS448 : Relational Database Systems'
+    }  
+}
+
+function cs471() {
+    const item = this.parentElement.querySelector('#cs471')
+    console.log(item.innerHTML)
+    if(item.innerHTML=='▾ CS471 : Introduction to Artificial Intelligence')
+    {
+        item.innerHTML = 
+        `
+            <v>▸ CS471 : Introduction to Artificial Intelligence
+            <h6 style="border-left: 1px solid lightgrey; padding-left: 5pt;"> Students are 
+            expected to spend at least three hours per week gaining experience with artificial 
+            intelligence systems and developing software. Basic problem-solving strategies, 
+            heuristic search, problem reduction and AND/OR graphs, knowledge representation, 
+            expert systems, generating explanations, uncertainty reasoning, game playing, 
+            planning, machine learning, computer vision, and programming systems such as 
+            Lisp or Prolog. </h6>
+        `
+    } else {
+        item.innerHTML='▾ CS471 : Introduction to Artificial Intelligence'
+    }  
+}
+
+function stat350() {
+    const item = this.parentElement.querySelector('#stat350')
+    console.log(item.innerHTML)
+    if(item.innerHTML=='▾ STAT350 : Introduction To Statistics')
+    {
+        item.innerHTML = 
+        `
+            <v>▸ STAT350 : Introduction To Statistics
+            <h6 style="border-left: 1px solid lightgrey; padding-left: 5pt;"> A data-oriented 
+            introduction to the fundamental concepts and methods of applied statistics. 
+            Exploratory analysis of data. Sample design and experimental design. Probability 
+            distributions and simulation. Sampling distributions. The reasoning of statistical 
+            inference. Confidence intervals and tests for one and two samples. Inference for 
+            contingency tables, regression, and correlation. Introduction to regression with 
+            several explanatory variables. Essential use is made of statistical software throughout. </h6>
+        `
+    } else {
+        item.innerHTML='▾ STAT350 : Introduction To Statistics'
+    }  
+}
+
+function stat416() {
+    const item = this.parentElement.querySelector('#stat416')
+    console.log(item.innerHTML)
+    if(item.innerHTML=='▾ STAT416 : Probability')
+    {
+        item.innerHTML = 
+        `
+            <v>▸ STAT416 : Probability
+            <h6 style="border-left: 1px solid lightgrey; padding-left: 5pt;"> An introduction 
+            to mathematical probability suitable as preparation for actuarial science, 
+            statistical theory, and mathematical modeling. General probability rules, conditional 
+            probability and Bayes theorem, discrete and continuous random variables, moments 
+            and moment generating functions, joint and conditional distributions, standard 
+            discrete and continuous distributions and their properties, law of large numbers 
+            and central limit theorem.  </h6>
+        `
+    } else {
+        item.innerHTML='▾ STAT416 : Probability'
     }  
 }
