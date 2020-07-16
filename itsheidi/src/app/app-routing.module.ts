e0f01624624e8c1fd11c09cd8a1b203726119554
Heidi Anderson <heidianderson@Heidis-MBP.curtisanderson1.com>
2020-07-16 15:37:33 -0700

    Added routing for Life, Work, and Home pages plus navbar.

--- itsheidi/src/app/app-routing.module.ts
+++ itsheidi/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LifeComponent } from './life/life.component';
+import { WorkComponent } from './work/work.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'life', component: LifeComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  getPath(): string {
+    return 'life';
+  }
+
+}
--- itsheidi/src/app/app.module.ts
+++ itsheidi/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LifeComponent } from './life/life.component';
+import { WorkComponent } from './work/work.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LifeComponent,
+    WorkComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
