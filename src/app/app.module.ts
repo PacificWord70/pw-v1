f968440fed6fa46ef0bbfa18df9549d46b7df5da
PacificWord70 <>
2020-09-15 22:06:24 -0700

    Fixed some formatting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TabModule } from 'angular-tabs-component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +35,6 @@ import { SchoolCoursesComponent } from './components/school-courses/school-cours
   ],
   imports: [
     BrowserModule,
-    TabModule,
     AppRoutingModule,
     NgbModule
   ],
